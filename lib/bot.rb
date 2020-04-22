9c3a7183db39f444eb0f589180b860854f2485b1
Shabab Ali <shababsaifi@gmail.com>
2020-04-22 19:06:10 +0530

    Added bot class with publish_tweet method

---
+++ lib/bot.rb
@@ -0,0 +1,11 @@
+class Bot
+  def publish_tweet(rest_client, text)
+      rest_client.update(text)
+  end
+
+  def search_tweet(rest_client)
+    rest_client.search('#rubybenefits').take(3).each do |tweet|
+      rest_client.update("@#{tweet.user.screen_name} Hey I love Ruby too, what are your favorite blogs? :)")
+    end
+  end
+end
--- lib/config.rb
+++ lib/config.rb
@@ -1,12 +1,17 @@
 require 'twitter'
 require 'dotenv/load'
-config = {
-  consumer_key: ENV['consumer_key'],
-  consumer_secret: ENV['consumer_secret'],
-  access_token: ENV['access_token'],
-  access_token_secret: ENV['access_token_secret']
-}
-client = Twitter::REST::Client.new(config)
 
-tweets = client.user_timeline('shabab_ali', count: 20)
-tweets.each { |tweet| puts tweet.full_text }
+class Config
+  def initialize
+    @config = {
+      consumer_key: ENV['consumer_key'],
+      consumer_secret: ENV['consumer_secret'],
+      access_token: ENV['access_token'],
+      access_token_secret: ENV['access_token_secret']
+    }
+  end
+
+  def rest_client
+    Twitter::REST::Client.new(@config)
+  end
+end
